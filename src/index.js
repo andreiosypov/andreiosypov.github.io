ba0bee9269fb37aee8f71e5d2c498bf813e2047a
aosypov <andrei@xrathus.com>
2021-12-10 15:34:12 -0600

    Undo experimenting with the basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from './App';
+import ArtGallery from './components/Pages/ArtGallery';
+import NotFound from './components/Pages/NotFound';
+import Contact from './components/Pages/Contact';
+
+import Artwork from './components/Tiles/Artwork';
+
+require('@elastic/eui/dist/eui_theme_light.css');
+
+const rootElement = document.getElementById('root');
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>}>
+        <Route path="gallery" element={<ArtGallery/>}>
+          <Route path=":artworkId" element={<Artwork/>}/>
+        </Route>
+        <Route path="contact" element={<Contact/>}/>
+      </Route>
+
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default Routes;
