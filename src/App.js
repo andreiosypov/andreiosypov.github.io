df416dfdd392c28e72c9bf09f9266977631fc3b6
aosypov <andrei@xrathus.com>
2021-12-10 15:21:15 -0600

    experimenting with the basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import Home from './components/Pages/Home';
-import './css/App.css';
-import './css/index.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/functions/Storyblok.js
+++
@@ -1,9 +0,0 @@
-import StoryblokClient from 'storyblok-js-client';
-
-export const Storyblok = new StoryblokClient({
-  accessToken: process.env.REACT_APP_STORYBLOK_TOKEN,
-  cache: {
-    clear: 'auto',
-    type: 'memory'
-  }
-});
