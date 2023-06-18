ce68dd8933dd6e6de44ab4009b2d1affeb359595
andreiosypov <andrei.osypov@brazenanimation.com>
2023-06-17 19:48:43 -0500

    gh-pages

---
+++ assets/NotFoundView-c3b2c484.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as n,a as o}from"./index-df47d585.js";const s={name:"NotFoundView"},c=o("h1",null,"404",-1),a=o("h2",null,"Not Found",-1),_=[c,a];function r(d,i,l,u,p,f){return t(),n("div",null,_)}const m=e(s,[["render",r]]);export{m as default};
